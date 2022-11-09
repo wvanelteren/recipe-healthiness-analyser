c2416b2ee3269eaa8c75720233e6ec6f5fd267e9
Wouter van Elteren <wvanelteren@gmail.com>
2022-11-09 15:25:58 +0100

    added methods

--- recipe_healthiness_analyser/recipe_analyser/nutriscore/nutriscore_analyser.py
+++ recipe_healthiness_analyser/recipe_analyser/nutriscore/nutriscore_analyser.py
@@ -8,7 +8,47 @@ class NutriScoreAnalyser:
         with open("nutriscore_table.json") as json_file:
             self.score_table = json.load(json_file)
 
-    def calculate():
+    def calculate_healthiness_score(
+        calories: float,
+        sugar: float,
+        saturated_fat: float,
+        sodium: float,
+        protein: float,
+        fiber: float,
+        percentage_fvn: float,
+    ) -> int:
+        raise NotImplementedError
+
+    def calculate_nutriscore(
+        calories: float,
+        sugar: float,
+        saturated_fat: float,
+        sodium: float,
+        protein: float,
+        fiber: float,
+        percentage_fvn: float,
+    ) -> str:
+        raise NotImplementedError
+
+    def calculate_calories_score(calories: float) -> int:
+        raise NotImplementedError
+
+    def calculate_sugar_score(sugar: float) -> int:
+        raise NotImplementedError
+
+    def calculate_saturated_fat_score(saturated_fat: float) -> int:
+        raise NotImplementedError
+
+    def calculate_sodium_score(sodium: float) -> int:
+        raise NotImplementedError
+
+    def calculate_protein_score(protein: float) -> int:
+        raise NotImplementedError
+
+    def calculate_fiber_score(fiber: float) -> int:
+        raise NotImplementedError
+
+    def calculate_fvn_score(percentage_fvn: float) -> int:
         raise NotImplementedError
 
     @staticmethod
